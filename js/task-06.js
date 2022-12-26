3796bb5e860a345d984cc5531dfbc1fa25e2eecb
Maksym-Babich <maxus1042@gmail.com>
2022-12-26 20:47:26 +0300

    removed console logging

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,6 @@
 const inputRef = document.querySelector('#validation-input');
 
 const onInputBlur = function () {
-  console.log(`inputRef.value.length = ${inputRef.value.length}`);
-  console.log(`inputRef.dataset.length = ${inputRef.dataset.length}`);
   if (`${inputRef.value.length}` === `${inputRef.dataset.length}`) {
     inputRef.classList.remove('invalid');
     inputRef.classList.add('valid');
